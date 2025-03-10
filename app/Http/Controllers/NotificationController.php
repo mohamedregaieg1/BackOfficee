8c94280e3c9c6eec8e8d0685a99e58765402f95c
mohamedregaieg1 <mohamedregaieg54057@gmail.com>
2025-03-10 22:09:25 +0100

    10-3-2025

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -11,12 +11,19 @@ class NotificationController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $notifications = Notification::where('receiver_id', $user->id)
-                            ->orderBy('created_at', 'desc')
-                            ->get();
+
+        $notifications = Notification::select(
+                'notifications.*',
+                'users.avatar_path as sender_avatar_path'
+            )
+            ->join('users', 'notifications.sender_id', '=', 'users.id')
+            ->where('notifications.receiver_id', $user->id)
+            ->orderBy('notifications.created_at', 'desc')
+            ->get();
 
         return response()->json($notifications);
     }
+
 
     public function markAsRead($id)
     {
